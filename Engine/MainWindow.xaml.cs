6e94f9137837a9f70cd05ac2ebab98b91307bd9b
Codie Shannon <codie.shannon.work@outlook.com>
2024-02-09 17:59:28 +1300

    Add project files.

---
+++ Engine/MainWindow.xaml.cs
@@ -0,0 +1,387 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using GleamTech.VideoUltimate;
+using System.IO;
+using System.Windows.Threading;
+
+namespace Engine
+{
+    public partial class MainWindow : Window
+    {
+        //Variables
+        //Wallpaper Window
+        public static WallpaperWindow WallpaperRef = new WallpaperWindow();
+        //File Browser
+        OpenFileDialog openFileDialog;
+        string InitialDirectory = "shell:MyComputerFolder";
+        string str_Importables = "Media (*.jpeg, *.jpg, *.png, *.tiff, *.psd, *.ai, *.raw, *.gif, *.avchd, *.avi, *.flv, *.mkv, *.mov, *.mp4, *.webm, *.wmv)|*.jpeg; *.jpg; *.png; *.tiff; *.psd; *.ai; *.raw; *.gif; *.JPEG; *.JPG; *.PNG; *.TIFF; *.PSD; *.AI; *.RAW; *.GIF; *.avchd; *.avi; *.flv; *.mkv; *.mov; *.mp4; *.webm; *.wmv; *.AVCHD; *.AVI; *.FLV; *.MKV; *.MOV; *.MP4; *.WEBM; *.WMV";
+        //Monitor Count
+        int monitorcounter;
+        DispatcherTimer dispatcherTimer;
+        bool[] isSet = new bool[] { false, false };
+        //Other
+        string[] VideoFormats = new string[] { "avchd", "avi", "flv", "mkv", "mov", "mp4", "webm", "wmv" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Setup Window
+            Setup();
+        }
+
+        public void Setup()
+        {
+            //Disable Wallpaper Button
+            btnWallpaper.IsEnabled = false;
+
+            if (System.Windows.Forms.Screen.AllScreens.Length == 1)
+            {
+                if (Properties.Settings.Default.isSet == true)
+                {
+                    //Load Wallpaper Information
+                    LoadInfo();
+                }
+            }
+            else
+            {
+                //Disable Browse Button
+                btnBrowse.IsEnabled = false;
+
+                //Change Wallpaper Availability Message
+                tbWallpaperAvailability.Text = "Monitor Count Unsupported";
+
+                //Show Message
+                tbWallpaperAvailability.Visibility = Visibility.Visible;
+
+                //Hide Wallpapers
+                meDisplayWallpaper.Visibility = Visibility.Collapsed;
+            }
+
+            //Check Monitor Count Every 5 Seconds
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(CheckMonitorCount_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 5);
+            dispatcherTimer.Start();
+        }
+
+        private void CheckMonitorCount_Tick(object sender, EventArgs e)
+        {
+            //Get Monitor Count
+            monitorcounter = System.Windows.Forms.Screen.AllScreens.Length;
+
+            if(monitorcounter == 1 && isSet[0] == false)
+            {
+                //Reset Message
+                tbWallpaperAvailability.Text = "No Wallpaper Available";
+
+                //Enable Browse Button
+                btnBrowse.IsEnabled = true;
+
+                //Load Info
+                LoadInfo();
+
+                //Set Bools
+                isSet[0] = true;
+                isSet[1] = false;
+            }
+            else if (monitorcounter > 1 && isSet[1] == false)
+            {
+                //Disable Browse Button
+                btnBrowse.IsEnabled = false;
+
+                //Change Wallpaper Availability Message
+                tbWallpaperAvailability.Text = "Monitor Count Unsupported";
+
+                //Show Message
+                tbWallpaperAvailability.Visibility = Visibility.Visible;
+
+                //Hide Wallpapers
+                HideWallpaper();
+                meDisplayWallpaper.Visibility = Visibility.Collapsed;
+
+                //Set Bools
+                isSet[1] = true;
+                isSet[0] = false;
+            }
+        }
+
+        #region Main Bar
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+        private void btnDetails_Click(object sender, RoutedEventArgs e)
+        {
+            //Hide Wallpaper Tab
+            btnWallpaper.IsEnabled = true;
+            WallpaperTab.Visibility = Visibility.Hidden;
+
+            //Show Details Tab
+            btnDetails.IsEnabled = false;
+            DetailsTab.Visibility = Visibility.Visible;
+        }
+
+        private void btnWallpaper_Click(object sender, RoutedEventArgs e)
+        {
+            //Hide Details Tab
+            btnDetails.IsEnabled = true;
+            DetailsTab.Visibility = Visibility.Hidden;
+
+            //Hide Wallpaper Tab
+            btnWallpaper.IsEnabled = false;
+            WallpaperTab.Visibility = Visibility.Visible;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            //Refresh Desktop
+            WallpaperWindow.SystemParametersInfo(WallpaperWindow.SPI_SETDESKWALLPAPER, 0, null, WallpaperWindow.SPIF_UPDATEINIFILE);
+            
+            //Close Application
+            Application.Current.Shutdown();
+        }
+        #endregion Main Bar
+
+        #region Wallpaper Body
+        private void meDisplayWallpaper_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            //Reset Position
+            meDisplayWallpaper.Position = TimeSpan.Zero;
+
+            //Play Video
+            meDisplayWallpaper.Play();
+        }
+
+        private void meDisplayWallpaper_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            //Reset To Defaults and Display Error Message
+            ResetDefaults();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            //Creating Open File Dialog
+            openFileDialog = new OpenFileDialog();
+
+            //Assign File Parameters
+            openFileDialog.InitialDirectory = InitialDirectory;
+            openFileDialog.Filter = str_Importables;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //Save Wallpaper Information
+                SaveInfo(openFileDialog.FileName);
+
+                //Load Wallpaper Information
+                LoadInfo();
+            }
+        }
+        #endregion Wallpaper Body
+
+        #region Main Methods
+        public void SaveInfo(string FileName)
+        {
+            try
+            {
+                //Save File Name
+                Properties.Settings.Default.FileName = Path.GetFileNameWithoutExtension(FileName);
+
+                //Save Path
+                Properties.Settings.Default.Path = openFileDialog.FileName;
+
+                //Save Format
+                Properties.Settings.Default.Format = Path.GetExtension(FileName).ToUpper().Replace(".", "");
+
+                //Save File Size
+                Properties.Settings.Default.FileSize = GetSize(new FileInfo(FileName).Length.ToString());
+
+                //Create Video Reader
+                VideoFrameReader VideoReader = new VideoFrameReader(FileName);
+
+                //Save Resolution
+                Properties.Settings.Default.Resolution = VideoReader.Width + " x " + VideoReader.Height;
+
+                //Save Duration
+                if (VideoFormats.Contains(Properties.Settings.Default.Format.ToLower()))
+                    Properties.Settings.Default.Duration = VideoReader.Duration.ToString(@"hh\:mm\:ss");
+                else
+                    Properties.Settings.Default.Duration = "N / A";
+
+                //Dispose of Video Reader
+                VideoReader.Dispose();
+
+                //Save Is Set Value
+                Properties.Settings.Default.isSet = true;
+
+                //Save Settings
+                Properties.Settings.Default.Save();
+            }
+            catch
+            {
+                //Reset To Defaults and Display Error Message
+                ResetDefaults();
+            }
+        }
+
+        public void LoadInfo()
+        {
+            try
+            {
+                // Wallpaper Page
+                // =====================================================================================
+                //Load Title
+                tbWallpaperTitle.Text = Properties.Settings.Default.FileName;
+
+                //Hide Wallpaper Availability Message
+                tbWallpaperAvailability.Visibility = Visibility.Collapsed;
+
+                //Show Display Wallpaper
+                meDisplayWallpaper.Visibility = Visibility.Visible;
+
+                //Load Display Wallpaper
+                meDisplayWallpaper.Source = new Uri(Properties.Settings.Default.Path, UriKind.Absolute);
+
+                //Play Display Wallpaper
+                meDisplayWallpaper.Play();
+
+                //Reset Display Path Color
+                tbDisplayPath.Foreground = Brushes.White;
+
+                //Load Display Path
+                tbDisplayPath.Text = Properties.Settings.Default.Path;
+
+
+                // Details Page
+                // ====================================================================================
+                //Set File Name
+                tbFileName.Text = Properties.Settings.Default.FileName;
+
+                //Set Path
+                tbPath.Text = Properties.Settings.Default.Path;
+
+                //Set Format
+                tbFormat.Text = Properties.Settings.Default.Format;
+
+                //Set File Size
+                tbFileSize.Text = Properties.Settings.Default.FileSize;
+
+                //Set Resolution
+                tbResolution.Text = Properties.Settings.Default.Resolution;
+
+                //Set Duration
+                tbDuration.Text = Properties.Settings.Default.Duration;
+
+
+                //Wallpaper Window
+                // ====================================================================================
+                //Parent Wallpaper Window
+                WallpaperWindow.ParentwWallpaper();
+
+                //Change Wallpaper
+                WallpaperWindow.ChangeWallpaper();
+
+                ShowWallpaper();
+            }
+            catch
+            {
+                //Reset To Defaults and Display Error Message
+                ResetDefaults();
+            }
+        }
+
+        public void ResetDefaults()
+        {
+            //Hide Wallpaper Window
+            HideWallpaper();
+
+            //Hide Display Wallpaper
+            meDisplayWallpaper.Visibility = Visibility.Collapsed;
+
+            //Show Wallpaper Availability Message
+            tbWallpaperAvailability.Visibility = Visibility.Visible;
+
+            //Reset Text
+            tbWallpaperTitle.Text = "Wallpaper Name";
+            tbDisplayPath.Text = "An error occured please select another wallpaper...";
+            tbDisplayPath.Foreground = Brushes.DarkRed;
+            tbFileName.Text = "N / A";
+            tbPath.Text = "N / A";
+            tbFormat.Text = "N / A";
+            tbFileSize.Text = "N / A";
+            tbResolution.Text = "N / A";
+            tbDuration.Text = "N / A";
+
+            //Reset Settings
+            Properties.Settings.Default.FileName = "";
+            Properties.Settings.Default.Path = "";
+            Properties.Settings.Default.Format = "";
+            Properties.Settings.Default.FileSize = "";
+            Properties.Settings.Default.Resolution = "";
+            Properties.Settings.Default.Duration = "";
+            Properties.Settings.Default.isAnimated = false;
+            Properties.Settings.Default.isSet = false;
+
+            //Save Settings
+            Properties.Settings.Default.Save();
+        }
+
+        string GetSize(string str_bytesize)
+        {
+            if (str_bytesize != "")
+            {
+                float float_bytesize = Convert.ToInt32(str_bytesize);
+
+                if (float_bytesize / 1024 / 1024 / 1024 / 1024 >= 1)
+                    return "" + Convert.ToInt32(float_bytesize / 1024 / 1024 / 1024 / 1024) + " TB";
+                else if (float_bytesize / 1024 / 1024 / 1024 >= 1)
+                    return "" + Convert.ToInt32(float_bytesize / 1024 / 1024 / 1024) + " GB";
+                else if (float_bytesize / 1024 / 1024 >= 1)
+                    return "" + Convert.ToInt32(float_bytesize / 1024 / 1024) + " MB";
+                else if (float_bytesize / 1024 >= 1)
+                    return "" + Convert.ToInt32(float_bytesize / 1024) + " KB";
+            }
+
+            return "No File Size Available";
+        }
+        #endregion Main Methods
+
+        public void ShowWallpaper()
+        {
+            //Show Wallpaper Window
+            WallpaperRef.Show();
+
+            //Refresh Desktop
+            WallpaperWindow.SystemParametersInfo(WallpaperWindow.SPI_SETDESKWALLPAPER, 0, null, WallpaperWindow.SPIF_UPDATEINIFILE);
+        }
+
+        public void HideWallpaper()
+        {
+            //Show Wallpaper Window
+            WallpaperRef.Hide();
+
+            //Refresh Desktop
+            WallpaperWindow.SystemParametersInfo(WallpaperWindow.SPI_SETDESKWALLPAPER, 0, null, WallpaperWindow.SPIF_UPDATEINIFILE);
+        }
+    }
+}
---
+++ Engine/WallpaperWindow.xaml.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Engine
+{
+    /// <summary>
+    /// Interaction logic for WallpaperWindow.xaml
+    /// </summary>
+    public partial class WallpaperWindow : Window
+    {
+        #region DLLs
+        #region Variables
+        //System Parameters Info Codes
+        public static UInt32 SPI_SETDESKWALLPAPER = 20;
+        public static UInt32 SPIF_UPDATEINIFILE = 0x1;
+        //Send Message Timeout Codes
+        public static UInt32 SPAWN_WORKERW = 0x052C;
+        public static UInt32 CLOSE = 0x0010;
+        public static uint Timeout = 1000;
+        #endregion Variables
+
+        #region Dll's
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string ClassName, string WindowName);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessageTimeout(IntPtr windowHandle, uint Msg, IntPtr wParam, IntPtr lParam, SendMessageTimeoutFlags flags, uint timeout, out IntPtr result);
+
+        [DllImport("user32.dll", SetLastError = true, EntryPoint = "GetWindow")]
+        public static extern IntPtr GetNextWindow(IntPtr hWnd, GetWindowType uCmd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        //[DllImport("user32.dll", SetLastError = true)]
+        //public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, SetWindowPosFlags uFlags);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern Int32 SystemParametersInfo(UInt32 uiAction, UInt32 uiParam, String pvParam, UInt32 fWinIni);
+        #endregion Dll's
+
+        #region Other
+        [Flags]
+        public enum SendMessageTimeoutFlags : uint
+        {
+            SMTO_NORMAL = 0x0,
+            SMTO_BLOCK = 0x1,
+            SMTO_ABORTIFHUNG = 0x2,
+            SMTO_NOTIMEOUTIFNOTHUNG = 0x8,
+            SMTO_ERRORONEXIT = 0x20
+        }
+
+        public enum GetWindowType : uint
+        {
+            /// <summary>
+            /// The retrieved handle identifies the window of the same type that is highest in the Z order.
+            /// <para/>
+            /// If the specified window is a topmost window, the handle identifies a topmost window.
+            /// If the specified window is a top-level window, the handle identifies a top-level window.
+            /// If the specified window is a child window, the handle identifies a sibling window.
+            /// </summary>
+            GW_HWNDFIRST = 0,
+            /// <summary>
+            /// The retrieved handle identifies the window of the same type that is lowest in the Z order.
+            /// <para />
+            /// If the specified window is a topmost window, the handle identifies a topmost window.
+            /// If the specified window is a top-level window, the handle identifies a top-level window.
+            /// If the specified window is a child window, the handle identifies a sibling window.
+            /// </summary>
+            GW_HWNDLAST = 1,
+            /// <summary>
+            /// The retrieved handle identifies the window below the specified window in the Z order.
+            /// <para />
+            /// If the specified window is a topmost window, the handle identifies a topmost window.
+            /// If the specified window is a top-level window, the handle identifies a top-level window.
+            /// If the specified window is a child window, the handle identifies a sibling window.
+            /// </summary>
+            GW_HWNDNEXT = 2,
+            /// <summary>
+            /// The retrieved handle identifies the window above the specified window in the Z order.
+            /// <para />
+            /// If the specified window is a topmost window, the handle identifies a topmost window.
+            /// If the specified window is a top-level window, the handle identifies a top-level window.
+            /// If the specified window is a child window, the handle identifies a sibling window.
+            /// </summary>
+            GW_HWNDPREV = 3,
+            /// <summary>
+            /// The retrieved handle identifies the specified window's owner window, if any.
+            /// </summary>
+            GW_OWNER = 4,
+            /// <summary>
+            /// The retrieved handle identifies the child window at the top of the Z order,
+            /// if the specified window is a parent window; otherwise, the retrieved handle is NULL.
+            /// The function examines only child windows of the specified window. It does not examine descendant windows.
+            /// </summary>
+            GW_CHILD = 5,
+            /// <summary>
+            /// The retrieved handle identifies the enabled popup window owned by the specified window (the
+            /// search uses the first such window found using GW_HWNDNEXT); otherwise, if there are no enabled
+            /// popup windows, the retrieved handle is that of the specified window.
+            /// </summary>
+            GW_ENABLEDPOPUP = 6
+        }
+
+        [Flags()]
+        public enum SetWindowPosFlags : uint
+        {
+            /// <summary>If the calling thread and the thread that owns the window are attached to different input queues,
+            /// the system posts the request to the thread that owns the window. This prevents the calling thread from
+            /// blocking its execution while other threads process the request.</summary>
+            /// <remarks>SWP_ASYNCWINDOWPOS</remarks>
+            AsynchronousWindowPosition = 0x4000,
+            /// <summary>Prevents generation of the WM_SYNCPAINT message.</summary>
+            /// <remarks>SWP_DEFERERASE</remarks>
+            DeferErase = 0x2000,
+            /// <summary>Draws a frame (defined in the window's class description) around the window.</summary>
+            /// <remarks>SWP_DRAWFRAME</remarks>
+            DrawFrame = 0x0020,
+            /// <summary>Applies new frame styles set using the SetWindowLong function. Sends a WM_NCCALCSIZE message to
+            /// the window, even if the window's size is not being changed. If this flag is not specified, WM_NCCALCSIZE
+            /// is sent only when the window's size is being changed.</summary>
+            /// <remarks>SWP_FRAMECHANGED</remarks>
+            FrameChanged = 0x0020,
+            /// <summary>Hides the window.</summary>
+            /// <remarks>SWP_HIDEWINDOW</remarks>
+            HideWindow = 0x0080,
+            /// <summary>Does not activate the window. If this flag is not set, the window is activated and moved to the
+            /// top of either the topmost or non-topmost group (depending on the setting of the hWndInsertAfter
+            /// parameter).</summary>
+            /// <remarks>SWP_NOACTIVATE</remarks>
+            DoNotActivate = 0x0010,
+            /// <summary>Discards the entire contents of the client area. If this flag is not specified, the valid
+            /// contents of the client area are saved and copied back into the client area after the window is sized or
+            /// repositioned.</summary>
+            /// <remarks>SWP_NOCOPYBITS</remarks>
+            DoNotCopyBits = 0x0100,
+            /// <summary>Retains the current position (ignores X and Y parameters).</summary>
+            /// <remarks>SWP_NOMOVE</remarks>
+            IgnoreMove = 0x0002,
+            /// <summary>Does not change the owner window's position in the Z order.</summary>
+            /// <remarks>SWP_NOOWNERZORDER</remarks>
+            DoNotChangeOwnerZOrder = 0x0200,
+            /// <summary>Does not redraw changes. If this flag is set, no repainting of any kind occurs. This applies to
+            /// the client area, the nonclient area (including the title bar and scroll bars), and any part of the parent
+            /// window uncovered as a result of the window being moved. When this flag is set, the application must
+            /// explicitly invalidate or redraw any parts of the window and parent window that need redrawing.</summary>
+            /// <remarks>SWP_NOREDRAW</remarks>
+            DoNotRedraw = 0x0008,
+            /// <summary>Same as the SWP_NOOWNERZORDER flag.</summary>
+            /// <remarks>SWP_NOREPOSITION</remarks>
+            DoNotReposition = 0x0200,
+            /// <summary>Prevents the window from receiving the WM_WINDOWPOSCHANGING message.</summary>
+            /// <remarks>SWP_NOSENDCHANGING</remarks>
+            DoNotSendChangingEvent = 0x0400,
+            /// <summary>Retains the current size (ignores the cx and cy parameters).</summary>
+            /// <remarks>SWP_NOSIZE</remarks>
+            IgnoreResize = 0x0001,
+            /// <summary>Retains the current Z order (ignores the hWndInsertAfter parameter).</summary>
+            /// <remarks>SWP_NOZORDER</remarks>
+            IgnoreZOrder = 0x0004,
+            /// <summary>Displays the window.</summary>
+            /// <remarks>SWP_SHOWWINDOW</remarks>
+            ShowWindow = 0x0040,
+        }
+        #endregion Other 
+        #endregion DLLs
+
+        //Windows
+        public static IntPtr wDesktop;
+        public static IntPtr wWorker;
+        public static IntPtr wWallpaper;
+
+        public WallpaperWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Run Parent Wallpaper Method
+            ParentwWallpaper();
+
+            //Assign Size
+            Height = SystemParameters.PrimaryScreenHeight;
+            Width = SystemParameters.PrimaryScreenWidth;
+
+            //Assign Position
+            Top = SystemParameters.VirtualScreenTop;
+            Left = SystemParameters.VirtualScreenLeft;
+        }
+
+        private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            //Set Wallpaper Position To 0
+            me_Wallpaper.Position = TimeSpan.FromSeconds(0);
+        }
+
+        public static void ParentwWallpaper()
+        {
+            //Get Program Manager Window
+            wDesktop = FindWindow("Progman", "Program Manager");
+
+            //Send Message To Program Manager To Spawn Worker
+            SendMessageTimeout(wDesktop, SPAWN_WORKERW, new IntPtr(0), IntPtr.Zero, SendMessageTimeoutFlags.SMTO_NORMAL, Timeout, out IntPtr result);
+
+            //Get wWorker
+            wWorker = GetNextWindow(wDesktop, GetWindowType.GW_HWNDPREV);
+
+            //Get Wallpaper Window
+            wWallpaper = new WindowInteropHelper(MainWindow.WallpaperRef).Handle;
+
+            //Set wWallpaper Parent To wWorker
+            SetParent(wWallpaper, wWorker);
+        }
+
+        public static void ChangeWallpaper()
+        {
+            MainWindow.WallpaperRef.me_Wallpaper.Source = new Uri(Properties.Settings.Default.Path);
+        }
+    }
+}
